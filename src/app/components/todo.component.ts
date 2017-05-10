e6b10b260420d0ce610f1ec0165792b844744368
d.litvinov <dmutro.litvinov@gmail.com>
2017-05-10 09:56:06 +0300

    finished app

--- src/app/components/todo.component.ts
+++ src/app/components/todo.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {TodoService} from './todo.service';
-import {Todo} from './todo';
+import { TodoService } from './todo.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'sg-todo',
